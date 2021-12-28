a74745e605f2f1c42f7fa955cc32c0cfd86bced3
WuYuanhun <1041747729@qq.com>
2021-12-29 01:47:53 +0800

    add SwerveAngleEncoder interface / abstract class

---
+++ src/main/java/frc/robot/common/BaseSwerveAngleEncoder.java
@@ -0,0 +1,20 @@
+package frc.robot.common;
+
+public abstract class BaseSwerveAngleEncoder implements SwerveAngleEncoder {
+    public double mOffset = 0;
+    public double mCof = 1;
+
+    @Override
+    public void setOffset(double s_offset) {
+        this.mOffset = s_offset;
+    }
+    
+    @Override
+    public double getPosition() {
+        return normalizeDegAngle((getRawOutput() - mOffset) * mCof);
+    }
+
+    public static double normalizeDegAngle(double angle) {
+        return angle - 360.0 * ((int) (angle / 360) + (angle < 0 ? -1 : 0));
+    }
+}
