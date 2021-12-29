14b6fbe73194ce99fd0c5b463b3a753518d917de
WuYuanhun <30903332+WuYuanhun@users.noreply.github.com>
2021-12-29 14:34:45 +0800

    complete SwerveDrive & SwerveModule

--- src/main/java/frc/robot/common/SwerveDrive.java
+++ src/main/java/frc/robot/common/SwerveDrive.java
@@ -4,8 +4,48 @@
 
 package frc.robot.common;
 
+import java.util.Arrays;
+
+import edu.wpi.first.wpilibj.Tracer;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
+
 /** Add your docs here. */
 public class SwerveDrive {
-    SwerveDrive[] mSwerveModules;
+    public final SwerveModule[] mSwerveModules;
+    public final SwerveDriveKinematics mKinematics;
+    public final Translation2d mCenterOfRotation;
 
+    public SwerveDrive(SwerveModule... swerveModules) {
+        this(new Translation2d(0, 0), swerveModules);
+    }
+
+    public SwerveDrive(Translation2d centerOfRotation, SwerveModule... swerveModules) {
+        if (swerveModules.length < 2) {
+            throw new IllegalArgumentException("A swerve drive requires at least two modules");
+        }
+        
+        mSwerveModules = swerveModules;
+        mKinematics = new SwerveDriveKinematics(
+            (Translation2d[]) Arrays.asList(mSwerveModules)
+                .stream()
+                .map((module) -> module.getModulePosition())
+                .toArray()
+            );
+            mCenterOfRotation = centerOfRotation;
+    }
+
+    public void setMotion(ChassisSpeeds desiredSpeed) {
+        setMotion(desiredSpeed, mCenterOfRotation);
+    }
+
+    public void setMotion(ChassisSpeeds desireSpeeds, Translation2d centerOfRotationMeters) {
+        SwerveModuleState[] moduleStates = mKinematics.toSwerveModuleStates(desireSpeeds, centerOfRotationMeters);
+        
+        for(int i = 0; i < moduleStates.length; i++) {
+            mSwerveModules[i].setState(moduleStates[i]);
+        }
+    }
 }
